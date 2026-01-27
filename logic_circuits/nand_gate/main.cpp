8081332514755b2d61dd7c7d38434bab0733d40f
Igor-Gabriel-D <igordantas.dev@gmail.com>
2026-01-27 09:49:57 -0300

    add nand gate

---
+++ logic_circuits/nand_gate/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main(){
+    bool A = false;
+    bool B = false;
+
+    cout << "| A | B | out |" << endl;
+    for(int i = 0; i < 2; i++){
+        for(int j = 0; j < 2; j++){
+            cout << "| " << A << " | " << B << " | " << ( (A && !B) || (!A && B) ) << "   |" << endl;
+	    B = !B;
+	}
+	A = !A;
+    }
+
+    return 0;
+}
